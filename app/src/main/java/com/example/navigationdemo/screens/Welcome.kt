ff8e913146b0d96515d0a43671ecb83942eac560
qyuiq <popow2281337@gmail.com>
2025-12-08 01:19:06 +0700

    edit Welcome.kt

--- app/src/main/java/com/example/navigationdemo/screens/Welcome.kt
+++ app/src/main/java/com/example/navigationdemo/screens/Welcome.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavKey
+import com.example.navigationdemo.ProfileScreen
+
 @Composable
 fun Welcome(onNavigation: (NavKey) -> Unit, name: String = "") {
     Box(
@@ -26,6 +28,7 @@ fun Welcome(onNavigation: (NavKey) -> Unit, name: String = "") {
             )
             Spacer(modifier = Modifier.size(30.dp))
             Button(onClick = {
+                onNavigation(ProfileScreen)
             }) {
                 Text(text = "Set up your Profile")
             }
