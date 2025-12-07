882e3f1c4bce9f2bc88c3083abf7d6ba0dbb9891
qyuiq <popow2281337@gmail.com>
2025-12-08 03:20:35 +0700

    fix some bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,9 @@ dependencies {
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.kotlinx.serialization.core)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.androidx.material3.adaptive.navigation3)
+    implementation(libs.kotlinx.serialization.core)
 }
